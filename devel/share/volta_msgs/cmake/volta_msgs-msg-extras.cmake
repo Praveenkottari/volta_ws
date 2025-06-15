86eef8f1933aa2b6c60f07edd7b3f6cae69b4d8f
Praveenkottari <112317600+Praveenkottari@users.noreply.github.com>
2025-06-15 16:22:05 +0530

    Delete devel directory

--- devel/share/volta_msgs/cmake/volta_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(volta_msgs_MESSAGE_FILES "/home/airl010/projects/volta_ws/src/volta/volta_msgs/msg/RPM.msg;/home/airl010/projects/volta_ws/src/volta/volta_msgs/msg/Diagnostics.msg;/home/airl010/projects/volta_ws/src/volta/volta_msgs/msg/Table.msg;/home/airl010/projects/volta_ws/src/volta/volta_msgs/msg/BMS.msg")
-set(volta_msgs_SERVICE_FILES "")
